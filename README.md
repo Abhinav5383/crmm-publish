abf8706f1fef0dfac52ff41c6eac8b2cc5b2f9ae
Abhinav <itsabhinav5383@gmail.com>
2024-11-03 21:51:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+## A simple script to publish your project versions to [CRMM](https://www.crmm.tech)
+
+This is a JS script so you will need a JS runtime installed on your pc like [Node.js](https://nodejs.org/en/download/package-manager) or [Bun](https://bun.sh/).
+
+`Config`
+```json
+{
+	"title": "Example version",
+	"version": "1.8.0",
+	"featured": false,
+	"releaseChannel": "release",
+	"loaders": ["quilt"],
+	"gameVersions": {
+		"file": "src/main/resources/quilt.mod.json",
+		"key": "quilt_loader.depends.1.versions"
+	},
+	"files": {
+		"primary": "dist/mod-1.8.0.jar",
+		"additional": ["dist/mod-sources-1.8.0.jar"]
+	},
+
+	"repoApi": "https://api.github.com/repos/USER_NAME/REPO",
+	"gitReleaseUrl": "/releases/latest",
+	"crmm": {
+		"authToken": "g5myuyngq3vsgu23afuuzorbecuebndhkbwckoy",
+		"projectId": "iris"
+	}
+}
+```
+
+- **title**: The title of the version (Optional, uses the `version` by default)
+
+- **loaders**: List of supported loaders (Required, [See available loaders here](https://api.crmm.tech/api/tags/loaders))
+
+- **gameVersions**: A list of supported game versions or an object containing path to a json and key to the game versions field in that json \
+    Example: 
+    ```json
+    "gameVersions": ["0.3.1", "0.3.2", "0.3.3", "0.3.4"],
+
+    // ----- OR -----
+
+    "gameVersions": {
+        "file": "src/main/resources/quilt.mod.json",
+        "key": "quilt_loader.depends.1.versions"
+    }
+
+    // NOTE:- You can use numbers for array indices
+    ```
+
+- **files**:
+    - **primary**: The primary file for the version (Required)
+    - **additional**: List of additional files (Optional, Max 10 files)
+
+- **crmm**:
+    - **authToken**: Your crmm authToken (Required, See below how to get that)
+    - **projectId**: The ID/Slug of the project you want to upload the version for
+
+
+### How to get the authToken
+
+*Please note that as of now there's no implementation of PATs, so we will be using your user session token for authentication.*
+
+- Visit the [api.crmm.tech](https://api.crmm.tech/api) website and open dev tools (press `ctrl` `shift` `i`), also make sure you are logged in else you'd need to [login](https://www.crmm.tech/login) first.
+- Go to the `network` tab
+- Refresh the page (`f5`)
+- Click on the first request made to the server
+- Scroll down in `headers` section and find "Cookie:"
+- Copy the `auth-token=YOUR_AUTH_TOKEN` part of the cookie.
+- There you have it, remove the `auth-token=` part and paste the rest in the config
---
+++ publish.js
@@ -0,0 +1,275 @@
+import { readFile } from "node:fs/promises";
+
+const CONFIG_FILE = "publish.config.json";
+const CRMM_API_URL = "https://api.crmm.tech/api";
+const includeGameVersionTypes = [
+	"release",
+	"beta",
+	"alpha",
+	// "pre-release",
+	// "snapshot"
+];
+
+async function main() {
+	const configData = await loadConfigData();
+	console.log("Config data loaded");
+	console.log({ ...configData, authToken: "REDACTED" });
+
+	console.log("Preparing form data...");
+	const formData = await prepareMultipartFormData(configData);
+
+	console.log("Uploading version...");
+	const response = await fetch(
+		`${CRMM_API_URL}/project/${configData.projectId}/version`,
+		{
+			method: "POST",
+			body: formData,
+			headers: {
+				Cookie: `auth-token=${configData.authToken}`,
+				Origin: "https://crmm.tech",
+				Referer: "https://crmm.tech/",
+			},
+		},
+	);
+
+	const data = await response.json();
+	console.log(data);
+}
+
+await main();
+
+async function prepareMultipartFormData(data) {
+	const formData = new FormData();
+	formData.append("title", data.title);
+	formData.append("changelog", data.changelog || "");
+	formData.append("featured", `${data.featured === true}`);
+	formData.append("releaseChannel", data.releaseChannel);
+	formData.append("versionNumber", data.version);
+	formData.append("loaders", JSON.stringify(data.loaders || []));
+	formData.append("gameVersions", JSON.stringify(data.gameVersions));
+	formData.append("dependencies", JSON.stringify(data.dependencies || []));
+	formData.append("primaryFile", data.primaryFile);
+	for (const additionalFile of data.additionalFiles || []) {
+		formData.append("additionalFiles", additionalFile);
+	}
+
+	return formData;
+}
+
+async function loadConfigData() {
+	console.log("Loading config...");
+	const config = await getConfig();
+
+	// Version
+	const version = config.version;
+	if (!version) {
+		throw new Error("Version is required");
+	}
+	const title = config.title || version;
+
+	const featured = config.featured === true;
+	const releaseChannel = config.releaseChannel || "release";
+	const loaders = config.loaders || [];
+	const gameVersions = getGameVersions(config.gameVersions);
+	if (!gameVersions) {
+		throw new Error("Game versions are required");
+	}
+
+	// Changelog
+	const repoUrl = config.repoApi;
+	const releasePath = config.gitReleaseUrl;
+
+	const releaseUrl = releasePath.startsWith("http")
+		? releasePath
+		: `${repoUrl}${releasePath}`;
+	const changelog = await getVersionChangelog(releaseUrl);
+
+	// Files
+	const primaryFilePath = config.files?.primary;
+	if (!primaryFilePath) {
+		throw new Error("Primary file path is required");
+	}
+
+	const primaryFile = await getFile(primaryFilePath);
+	const additionalFiles = await getFiles(config.files?.additional || []);
+
+	// Crmm API data
+	const authToken = config?.crmm?.authToken;
+	if (!authToken) throw new Error("Auth token is required");
+
+	const projectId = config?.crmm?.projectId;
+	if (!projectId) throw new Error("Project ID is required");
+
+	return {
+		title,
+		version,
+		featured,
+		releaseChannel,
+		loaders,
+		gameVersions,
+		changelog,
+		primaryFile,
+		additionalFiles,
+		authToken,
+		projectId,
+	};
+}
+
+async function getGameVersions(source) {
+	if (Array.isArray(source)) return source;
+
+	// The API returns sorted versions list in descending order
+	const res = await fetch(`${CRMM_API_URL}/tags/game-versions`);
+	const availableVersions = await res.json();
+
+	if (!availableVersions || !Array.isArray(availableVersions))
+		throw new Error("Failed to fetch game versions");
+
+	const filteredAvailableVersions = availableVersions
+		.filter((version) => includeGameVersionTypes.includes(version.releaseType))
+		.map((version) => version.value);
+
+	const gameVersionSourceKey = source.key;
+	if (!gameVersionSourceKey)
+		throw new Error("Missing field 'key' in 'gameVersions'");
+	const keys = gameVersionSourceKey.split(".");
+
+	const gameVersionSourceFile = await getFileContents(source.file, "utf-8");
+	const gameVersionSourceJson = JSON.parse(gameVersionSourceFile);
+
+	let gameVersions_raw = getObjValue(gameVersionSourceJson, keys);
+	if (!gameVersions_raw)
+		throw new Error(
+			`Failed to get game versions from source file using key '${gameVersionSourceKey}'`,
+		);
+
+	if (!Array.isArray(gameVersions_raw)) gameVersions_raw = [gameVersions_raw];
+
+	const parsedGameVersions = new Set();
+	for (const version of gameVersions_raw) {
+		const parsedList = parseVersion(version, filteredAvailableVersions);
+		if (!parsedList) continue;
+
+		for (const v of parsedList) {
+			parsedGameVersions.add(v);
+		}
+	}
+
+	const gameVersionsList = sortVersions(
+		Array.from(parsedGameVersions),
+		filteredAvailableVersions,
+	);
+	if (gameVersionsList.length === 0) throw new Error("No valid game versions");
+
+	return gameVersionsList;
+}
+
+async function getVersionChangelog(url) {
+	console.log("Fetching changelog...");
+
+	const res = await fetch(url);
+	const data = await res.json();
+
+	return data?.body || "";
+}
+
+async function getConfig() {
+	const file = await getFileContents(CONFIG_FILE, "utf-8");
+	const json = JSON.parse(file);
+
+	return json;
+}
+
+async function getFiles(paths) {
+	const files = await Promise.all(paths.map((path) => getFile(path)));
+	return files;
+}
+
+async function getFile(path) {
+	const fileContents = await getFileContents(path);
+	const fileName = path.split("/").pop();
+
+	return new File([fileContents], fileName);
+}
+
+async function getFileContents(path, encoding) {
+	const fileContents = await readFile(path, { encoding: encoding });
+	return fileContents;
+}
+
+function parseVersion(version, refList) {
+	let versionStr = version;
+	if (versionStr.startsWith("v")) versionStr = versionStr.slice(1);
+
+	// If the version string doesn't include any operators, return it as is
+	if (
+		!versionStr.includes("=") &&
+		!versionStr.includes("<") &&
+		!versionStr.includes(">")
+	) {
+		return versionStr;
+	}
+
+	// Throw error if the version string doesn't start with a valid operator but includes one
+	if (
+		!versionStr.startsWith("=") &&
+		!versionStr.startsWith(">=") &&
+		!versionStr.startsWith("<=") &&
+		!versionStr.startsWith(">") &&
+		!versionStr.startsWith("<")
+	) {
+		throw new Error(`Cannot parse version string: '${versionStr}'`);
+	}
+
+	const parsedVersions = [];
+	if (versionStr.startsWith("<")) {
+		versionStr = versionStr.slice(1);
+		if (versionStr.startsWith("=")) {
+			versionStr = versionStr.slice(1);
+			parsedVersions.push(versionStr);
+		}
+
+		const refIndex = refList.indexOf(versionStr);
+		if (refIndex === -1) return null;
+
+		parsedVersions.push(...refList.slice(refIndex + 1));
+	}
+
+	if (versionStr.startsWith(">")) {
+		versionStr = versionStr.slice(1);
+		if (versionStr.startsWith("=")) {
+			versionStr = versionStr.slice(1);
+			parsedVersions.push(versionStr);
+		}
+
+		const refIndex = refList.indexOf(versionStr);
+		if (refIndex === -1) return null;
+
+		parsedVersions.push(...refList.slice(0, refIndex));
+	}
+
+	if (versionStr.startsWith("=")) {
+		versionStr = versionStr.slice(1);
+		parsedVersions.push(versionStr);
+	}
+
+	return parsedVersions;
+}
+
+function sortVersions(list, refList) {
+	const sorted = [];
+	for (const version of refList) {
+		if (list.includes(version)) sorted.push(version);
+	}
+
+	return sorted;
+}
+
+function getObjValue(obj, keys) {
+	let curr = obj;
+	for (const key of keys) {
+		curr = curr[key];
+	}
+
+	return curr;
+}
